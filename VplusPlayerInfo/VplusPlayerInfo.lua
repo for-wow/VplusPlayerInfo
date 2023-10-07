dd45c6d9eafed3e2cc13fd2aecd64f2a789018dc
MaxWayborn <55049724+MaxWayborn@users.noreply.github.com>
2023-10-07 17:43:28 +0300

    Update VplusPlayerInfo.lua

--- VplusPlayerInfo/VplusPlayerInfo.lua
+++ VplusPlayerInfo/VplusPlayerInfo.lua
@@ -1,63 +1,54 @@
-CreateFrame('FRAME', 'VPHT_Frame', UIParent)
-VPHT_Frame:SetBackdrop({
+CreateFrame('FRAME', 'VPPI')
+VPPI:SetBackdrop({
     bgFile = 'Interface\\DialogFrame\\UI-DialogBox-Background', 
     edgeFile = 'Interface\\Tooltips\\UI-Tooltip-Border',
     tile = true, tileSize = 16, edgeSize = 16, 
     insets = { left = 4, right = 4, top = 4, bottom = 4 }
 })
-VPHT_Frame:SetPoint('CENTER', UIParent)
-VPHT_Frame:SetWidth(120)
-VPHT_Frame:SetHeight(60)
-VPHT_Frame:SetMovable(true)
-VPHT_Frame:EnableMouse(true)
-VPHT_Frame:RegisterForDrag('LeftButton')
-VPHT_Frame:SetScript('OnDragStart', function() this:StartMoving() end)
-VPHT_Frame:SetScript('OnDragStop',  function() this:StopMovingOrSizing() end)
-VPHT_Frame:Show()
+VPPI:SetPoint('CENTER', UIParent)
+VPPI:SetWidth(160)
+VPPI:SetHeight(52)
+VPPI:SetMovable(true)
+VPPI:EnableMouse(true)
+VPPI:RegisterForDrag('LeftButton')
+VPPI:SetScript('OnDragStart', function() this:StartMoving() end)
+VPPI:SetScript('OnDragStop', function() this:StopMovingOrSizing() end)
+VPPI:Show()
 
-CreateFrame('CheckButton', 'VPHT_Check', VPHT_Frame, 'UICheckButtonTemplate')
-VPHT_Check:SetPoint('TOPLEFT', VPHT_Frame)
-VPHT_Check:SetCheckedTexture('Interface\\Buttons\\UI-CheckBox-SwordCheck')
-VPHT_Check:GetCheckedTexture():SetPoint('TOPLEFT', VPHT_Check, 8, -10)
+VPPI.Check = CreateFrame('CheckButton', nil, VPPI, 'UICheckButtonTemplate')
+VPPI.Check:SetPoint('TOPLEFT', VPPI)
+VPPI.Check:SetCheckedTexture('Interface\\Buttons\\UI-CheckBox-SwordCheck')
+VPPI.Check:GetCheckedTexture():SetPoint('TOPLEFT', VPPI.Check, 8, -10)
 
-VPHT_Frame:CreateFontString('VPHT_ValueTimer', 'OVERLAY', 'GameFontHighlight')
-VPHT_ValueTimer:SetPoint('LEFT', VPHT_Check, 'RIGHT')
+VPPI.ValueTimer = VPPI:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
+VPPI.ValueTimer:SetPoint('LEFT', VPPI.Check, 'RIGHT')
 
-VPHT_Frame:CreateFontString('VPHT_TitleTimer', 'OVERLAY', 'GameFontHighlight')
-VPHT_TitleTimer:SetPoint('LEFT', VPHT_ValueTimer, 'RIGHT')
-VPHT_TitleTimer:SetText('- Timer')
+VPPI.TitleTimer = VPPI:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
+VPPI.TitleTimer:SetPoint('LEFT', VPPI.ValueTimer, 'RIGHT')
+VPPI.TitleTimer:SetText('   - Timer')
 
-VPHT_Frame:CreateFontString('VPHT_ValueEvent', 'OVERLAY', 'GameFontHighlight')
-VPHT_ValueEvent:SetPoint('LEFT', VPHT_Check, 'RIGHT', 0, -20)
+VPPI.ValueEvent = VPPI:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
+VPPI.ValueEvent:SetPoint('LEFT', VPPI.Check, 'RIGHT', 0, -20)
 
-VPHT_Frame:CreateFontString('VPHT_TitleEvent', 'OVERLAY', 'GameFontHighlight')
-VPHT_TitleEvent:SetPoint('LEFT', VPHT_ValueEvent, 'RIGHT')
-VPHT_TitleEvent:SetText('- Event')
+VPPI.TitleEvent = VPPI:CreateFontString(nil, 'OVERLAY', 'GameFontHighlight')
+VPPI.TitleEvent:SetPoint('LEFT', VPPI.ValueEvent, 'RIGHT')
+VPPI.TitleEvent:SetText('   - Event')
 
-
-
-VPHT_Check:SetScript('OnClick', function()
+VPPI.Check:SetScript('OnClick', function()
     if this:GetChecked() then
-        VPHT_ValueTimer:SetFontObject('GameFontHighlight')
-        VPHT_Frame:SetScript('OnUpdate', HastTest)
+        VPPI.ValueTimer:SetFontObject('GameFontHighlight')
+        VPPI:SetScript('OnUpdate', function()
+            local mainSpeed, offSpeed = UnitAttackSpeed('player')
+            VPPI.ValueTimer:SetText(string.format('%0.2f / %0.2f', mainSpeed, offSpeed or 0))
+        end)
     else
-        VPHT_ValueTimer:SetFontObject('GameFontDisable')
-        VPHT_Frame:SetScript('OnUpdate', nil)
+        VPPI.ValueTimer:SetFontObject('GameFontDisable')
+        VPPI:SetScript('OnUpdate', nil)
     end
 end)
 
-function HastTest()
+VPPI:RegisterEvent('UNIT_ATTACK_SPEED')
+VPPI:SetScript('OnEvent', function()
     local mainSpeed, offSpeed = UnitAttackSpeed('player')
-    VPHT_ValueTimer:SetText(string.format('%0.2f', mainSpeed))
-end
-
-
-
-VPHT_Frame:RegisterEvent('UNIT_DAMAGE')
-VPHT_Frame:RegisterEvent('PLAYER_DAMAGE_DONE_MODS')
-VPHT_Frame:RegisterEvent('UNIT_ATTACK_SPEED')
-VPHT_Frame:RegisterEvent('UNIT_RANGEDDAMAGE')
-VPHT_Frame:SetScript('OnEvent', function()
-    local mainSpeed, offSpeed = UnitAttackSpeed('player')
-    VPHT_ValueEvent:SetText(string.format('%0.2f', mainSpeed))
+    VPPI.ValueEvent:SetText(string.format('%0.2f / %0.2f', mainSpeed, offSpeed or 0))
 end)
